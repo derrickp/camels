bb8602cd95d5acd03c4fb47a480ae6a5dc6da259
Derrick Plotsky <derrickplotsky@gmail.com>
2018-11-11 18:33:02 -0800

    Remove unnecessary test
    This was just a placeholder. Can be removed.

--- spec/camels_spec.rb
+++ spec/camels_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Camels do
   it 'has a version number' do
     expect(Camels::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
